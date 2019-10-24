f3d0fbf80cc0e6bbfb705a8aac29fb687dd38f27
doyle-flutter <56661529+doyle-flutter@users.noreply.github.com>
2019-10-24 20:49:21 +0900

    191024
    191024

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.get("/", (req, res) => {
+    return res.send("asasd");
+});
+
+app.get("/item/all", (req, res) =>{
+    let datas = fs.readFileSync("shoppingData.json");
+    jsonDatas = JSON.parse(datas);
+    console.log(jsonDatas);
+    let result = jsonDatas;
+    return res.json(result);
+});
+
+app.get("/item/:id", (req, res) =>{
+    let id = req.params.id;
+    let datas = fs.readFileSync("shoppingData.json");
+    jsonDatas = JSON.parse(datas);
+    console.log(jsonDatas);
+    let result = jsonDatas[id];
+    return res.json(result);
+});
+
+app.post("/buy/item/:id", (req, res) =>{
+    let id = req.params.id;
+    let datas = fs.readFileSync("shoppingData.json");
+    jsonDatas = JSON.parse(datas);
+    let result = jsonDatas[id];
+    if(result.ea == 0){
+        return res.sendStatus(404);
+    }
+    result.ea -= 1;
+    console.log(result);
+    jsonDatas[id] = result;
+    fs.writeFileSync("shoppingData.json", JSON.stringify(jsonDatas),'utf-8');
+    return res.sendStatus(200);
+    
+});
+
+app.listen(3000, (err)=>{
+    console.log("connect 3000");
+});
